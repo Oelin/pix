ab8d59bf0e9ff7fca930903862c40bbae93a1e50
Oelin <42823429+Oelin@users.noreply.github.com>
2020-06-06 10:42:44 +0100

    Update spp.py

--- asm/spp.py
+++ asm/spp.py
@@ -1,4 +1,5 @@
 # spp.py, a simulated pixy processor to run pixy binaries.
 
-# some notes
+# notes:
 # circuitverse has a maxium RAM size of 1Mx32b which is a 4MB python array.
+# stdout is memory-mapped at address 0x0000.
