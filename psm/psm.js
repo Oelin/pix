3e483825b40f18240ff1e91d91acc83ed0bdea89
Oelin <42823429+Oelin@users.noreply.github.com>
2020-09-08 17:38:32 +0100

    Update psm.js

--- psm/psm.js
+++ psm/psm.js
@@ -1,14 +1,12 @@
 // psm.js, the pixy assembler
 
 
-
 const white = /^\s*[\r\n]+/gm
 const eolb = /:[\n\s]+/g
 const note = /#.*/g
 
 
-
 function prune(src) {
-	return src.replaceAll(white, '')
-	.replaceAll(eolb, ':')
+  return src.replaceAll(white, '')
+  .replaceAll(eolb, ':')
 }
