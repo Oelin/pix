d6ae0840012bb865647a5434a528c8b127b1bead
Oelin <42823429+Oelin@users.noreply.github.com>
2020-09-08 17:37:54 +0100

    Update psm.js

--- psm/psm.js
+++ psm/psm.js
@@ -2,69 +2,13 @@
 
 
 
-// opcode table
-
-const opcodes = [
-  'add r r',
-  'add r v',
-  'sub r r',
-  'sub r v',
-  'and r r',
-  'or r r',
-  'xor r r',
-  'not r',
-  'b v',
-  'mov r r',
-  'mov r v',
-  'up r v',
-  'sto v r',
-  'sto r r'
-]
+const white = /^\s*[\r\n]+/gm
+const eolb = /:[\n\s]+/g
+const note = /#.*/g
 
 
 
-// preprocessors
-
-const purify = code =>
-  code.split('\n')
-  .filter(line => line && !line.startsWith('#'))
-  .map(line => line.trim())
-  .join('\n')
-  .replaceAll(/:[\n\s]+/g, ':')
-
-
-
-const names = code =>
-  code.split('\n')
-  .map(line => line.match(/^(\w+):/))
-  .map((match, num) => match && [match[1], num])
-  .filter(Boolean)
-
-
-
-function sub(code, name, num) {
-  return code.replaceAll(`$${name}`, num)
-  .replace(`${name}:`, '')
-}
-
-
-
-// parser
-
-function* parse(lines) {
-
-}
-
-
-
-// assemble to machine code 
-
-function asm(parsed) {
-
-}
-
-
-
-const main = code => {
-
+function prune(src) {
+	return src.replaceAll(white, '')
+	.replaceAll(eolb, ':')
 }
