31e676ad0ea62b14210a21168fd7fd3ca69ec3cd
Oelin <42823429+Oelin@users.noreply.github.com>
2020-09-09 16:13:21 +0100

    Update psm.js

--- psm/psm.js
+++ psm/psm.js
@@ -1,6 +1,4 @@
 // psm.js, the pixy assembler
-
-
 let white = /^\s*[\r\n]+|#.*/gm
 let name = /^([a-zA-Z_][\w_]*):/
 let nend = /:[\n\s]+/g
@@ -8,26 +6,44 @@ let nend = /:[\n\s]+/g
 
 function prune(src) {
   return src.replaceAll(white, '')
-  .replaceAll(nend, ':').trim()
+    .replaceAll(nend, ':').trim()
 }
 
 
 function names(src) {
   let m
 
-  return src.map(line =>
-    (m = line.match(name)) && m[1])
-  
-  .filter(m => m)
+  return src.map((line, n) =>
+      (m = line.match(name)) && [m[1], n])
+
+    .filter(m => m)
 }
 
 
 function parse(src) {
-  //return src.
+  // return srcz	
 }
 
 
 function asm(parsed) {
   // * given the opcode, rn, rs and val, produce the data for an instruction
   // * or, given an item of data, convert it to some binary representation.
+}
+
+
+function main(src) {
+  src = prune(src)
+
+  // replace names 
+
+  names(src.split('\n')).forEach(([name, n]) =>
+    src = src.replaceAll(`$${name}`, n)
+    .replaceAll(`${name}:`, '')
+  )
+
+  // parse src
+  // assemble parsed
+  // return 
+
+  return src
 }
